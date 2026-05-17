dc0ea3478cc5f0570d2fd8a304730ad7f8551956
krishnagp97 <krishnapathak4511@gmail.com>
2026-05-17 21:06:09 +0530

    setup authentication with Better Auth and add sign in/sign up pages

---
+++ components/image-tab.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+import {Button} from "@/components/ui/button"
+
+export default function ImageTab(){
+    const [activeTab, setActiveTab] = useState("organize");
+    return (
+        <section className="border-t bg-white py-16">
+          <div className="container mx-auto px-4">
+            <div className="mx-auto max-w-6xl">
+              <div className="flex gap-2 justify-center b-8">
+                <Button onClick={()=> setActiveTab("organize")} className={`rounded-lg px-6 p-3 text-sm font-medium transition-colors ${activeTab === "organize" ? "bg-primary text-white" : "bg-gray-100 text-gray-700 hover:bg-gray-200"}`}>Organize Applications</Button>
+                <Button onClick={()=> setActiveTab("hired")} className={`rounded-lg px-6 p-3 text-sm font-medium transition-colors ${activeTab === "hired" ? "bg-primary text-white" : "bg-gray-100 text-gray-700 hover:bg-gray-200"}`}>Get Hired</Button>
+                <Button onClick={()=> setActiveTab("boards")} className={`rounded-lg px-6 p-3 text-sm font-medium transition-colors ${activeTab === "boards" ? "bg-primary text-white" : "bg-gray-100 text-gray-700 hover:bg-gray-200"}`}>Manage Boards</Button>
+              </div>
+            </div>
+            <div className="relative mx-auto max-w-5xl overflow-hidden rounded-lg border border-gray-200 shadow-xl">
+              {activeTab === "organize" && (
+                <Image
+                  src="/hero-image/hero1.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800}
+                />
+              )}
+
+              {activeTab === "hired" && (
+                <Image
+                  src="/hero-image/hero2.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800}
+                />
+              )}
+
+              {activeTab === "boards" && (
+                <Image
+                  src="/hero-image/hero3.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800}
+                />
+              )}
+            </div>
+          </div>
+        </section>
+
+    )
+}
